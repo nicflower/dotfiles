81f81acbaf2b433e1e9b6d43e791d95a89aa8167
Nicola Fiorella <nicola.fiorella@gmail.com>
2024-02-10 18:07:58 +0100

    move blankline plugin to utils

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -3,5 +3,13 @@ return {
     {
         "folke/which-key.nvim",
         opts = {},
-    }
+    },
+    {
+        -- Add indentation guides even on blank lines
+        'lukas-reineke/indent-blankline.nvim',
+        -- Enable `lukas-reineke/indent-blankline.nvim`
+        -- See `:help ibl`
+        main = 'ibl',
+        opts = {},
+    },
 }
